b5d337282180e7eb3a98cc2a48bbd3d5b95f9ed3
Jason Chaffee <jchaffee@sunverge.com>
2015-04-30 03:49:47 -0700

    added a bunch of zsh files with extra configuration and nicely organized

---
+++ zsh/export.zsh
@@ -0,0 +1,12 @@
+# Setup terminal, and turn on colors
+export TERM=xterm-256color
+export CLICOLOR=1
+export LSCOLORS=Gxfxcxdxbxegedabagacad
+
+# Enable color in grep
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='3;33'
+
+#export LESS='--ignore-case --raw-control-chars'
+#export PAGER='less'
+export EDITOR='vim'
