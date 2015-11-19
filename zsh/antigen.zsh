f65cf9750a17a26c889b78ab5214a006bc00ddb5
Jason Chaffee <jason@peek.com>
2015-11-18 17:46:28 -0800

    added more plugins

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -9,15 +9,28 @@ if [[ -f ~/.antigen/antigen.zsh ]]; then
     antigen bundle sorin-ionescu/prezto
 
     # Bundles from the default repo (robbyrussell's oh-my-zsh).
+    antigen bundle bower
     antigen bundle brew
     antigen bundle brew-cask
+    antigen bundle bundler
     antigen bundle docker
+    antigen bundle docker-compose
+    antigen bundle ember-cli
     antigen bundle gem
     antigen bundle git
+    antigen bundle go
+    antigen bundle history
     antigen bundle mvn
     antigen bundle node
     antigen bundle npm
+    antigen bundle nvm
     antigen bundle pip
+    antigen bundle postgres
+    antigen bundle rails
+    antigen bundle rake
+    antigen bundle rbenv
+    antigen bundle ruby
+    antigen bundle safe-paste
     antigen bundle sbt
     antigen bundle scala
     antigen bundle svn
