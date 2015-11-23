4f1df7c6fcbd8887cd40b35fbb538244a063333f
Jason Chaffee <jason@peek.com>
2015-11-23 12:45:30 -0800

    added git_up function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -333,3 +333,20 @@ precmd() {
   # sets the tab title to current dir
   echo -ne "\e]1;${PWD##*/}\a"
 }
+
+# -------------------------------------------------------------------
+# Run git up recursively over the specified directory
+#
+# -------------------------------------------------------------------
+git_up() {
+  PROJECT_DIR=$1
+
+  for directory in `find ${PROJECT_DIR} -type d`; do
+    if [ -d ${directory}/.git ]; then
+      pushd $directory;
+      echo $directory;
+      git up;
+      popd;
+    fi
+  done
+}
