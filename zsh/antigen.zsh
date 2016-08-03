f25c7fb95e408894dbceb320287109efa97a595c
Jason Chaffee <jason@gearlaunch.com>
2016-08-02 20:30:17 -0700

    removed prezto

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -9,33 +9,7 @@ if [[ -f ${HOME}/.antigen/antigen.zsh ]]; then
     #antigen bundle sorin-ionescu/prezto
 
     # Bundles from the default repo (robbyrussell's oh-my-zsh).
-    antigen bundle bower
-    antigen bundle brew
-    antigen bundle brew-cask
-    antigen bundle bundler
-    antigen bundle docker
-    antigen bundle docker-compose
-    antigen bundle ember-cli
-    antigen bundle gem
-    antigen bundle git
-    antigen bundle go
-    antigen bundle history
-    antigen bundle mvn
-    antigen bundle node
-    antigen bundle npm
-    antigen bundle nvm
-    antigen bundle pip
-    antigen bundle postgres
-    antigen bundle rails
-    antigen bundle rake
-    antigen bundle rbenv
-    antigen bundle ruby
-    antigen bundle safe-paste
-    antigen bundle sbt
-    antigen bundle scala
-    antigen bundle svn
-    antigen bundle tmux
-    antigen bundle command-not-found
+
 
     # Syntax highlighting bundle.
     antigen bundle zsh-users/zsh-syntax-highlighting
