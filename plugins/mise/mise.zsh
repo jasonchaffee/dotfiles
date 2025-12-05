c0fa64a62cd0cec1435943a6fddbc3a1c5fb9c5a
jchaffee <jchaffee@paypal.com>
2025-12-05 11:21:40 -0800

    Remove export of MISE_VERBOSE from mise.zsh

--- plugins/mise/mise.zsh
+++ plugins/mise/mise.zsh
@@ -3,6 +3,5 @@
 # -------------------------------------------------------------------
 export SHELL_NAME=$(basename "$SHELL")
 if command -v mise >/dev/null 2>&1; then;
-    export MISE_USE_CURL=1
     eval "$(mise activate ${SHELL_NAME})"
 fi
