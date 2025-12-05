12fac43578a274eed203a653c16764df8226b694
jchaffee <jchaffee@paypal.com>
2025-12-05 10:31:14 -0800

    Update curl.zsh to use system curl instead of mise curl

--- plugins/curl/curl.zsh
+++ plugins/curl/curl.zsh
@@ -2,7 +2,7 @@ if [[ $(uname) = 'Darwin' ]]; then
     if command -v brew >/dev/null 2>&1; then
         if command -v /opt/homebrew/opt/curl/bin/curl >/dev/null 2>&1; then
             export PATH="/opt/homebrew/opt/curl/bin:$PATH"
-            export CURL_CA_BUNDLE="/opt/homebrew/etc/openssl/cert.pem"
+            #export CURL_CA_BUNDLE="/opt/homebrew/etc/ca-certificates/cert.pem"
         fi
     fi
 fi
