133afa6295b2b338f6142689949272be5b4c3bca
jchaffee <jchaffee@paypal.com>
2025-12-05 09:23:24 -0800

    Add mise configuration to use system curl instead of mise curl

--- plugins/mise/mise.zsh
+++ plugins/mise/mise.zsh
@@ -2,6 +2,7 @@
 # Activate mise
 # -------------------------------------------------------------------
 export SHELL_NAME=$(basename "$SHELL")
-if command -v mise >/dev/null 2>&1; then eval "$(mise activate ${SHELL_NAME})"; fi
-
-export MISE_USE_CURL=1
+if command -v mise >/dev/null 2>&1; then;
+    export MISE_USE_CURL=1
+    eval "$(mise activate ${SHELL_NAME})"
+fi
