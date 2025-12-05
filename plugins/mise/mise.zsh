4f501b748f466d9e8278981eee895884e8e3a576
jchaffee <jchaffee@paypal.com>
2025-12-05 09:37:46 -0800

    Add MISE_VERBOSE to mise.zsh

--- plugins/mise/mise.zsh
+++ plugins/mise/mise.zsh
@@ -4,5 +4,6 @@
 export SHELL_NAME=$(basename "$SHELL")
 if command -v mise >/dev/null 2>&1; then;
     export MISE_USE_CURL=1
+    export MISE_VERBOSE=1
     eval "$(mise activate ${SHELL_NAME})"
 fi
